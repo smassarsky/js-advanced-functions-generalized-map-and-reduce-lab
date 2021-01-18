c613e77eef5c268e5594448977ec9694fee42da8
smassarsky <seth.massarsky@gmail.com>
2021-01-18 16:04:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(sourceArray, func) {
+  let out = []
+  for (let element of sourceArray) {
+    out.push(func(element))
+  }
+  return out
+}
+
+function reduce(sourceArray, func, startingPoint) {
+  let i
+  if (!startingPoint) {
+    startingPoint = sourceArray[0]
+    i = 1
+  } else {
+    i = 0
+  }
+  for (; i < sourceArray.length; i++) {
+    startingPoint = func(sourceArray[i], startingPoint)
+  }
+  return startingPoint
+}
